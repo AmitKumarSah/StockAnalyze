5a0801cb9c5d6a88b5c0405073b3bf1b07882beb
Tomas Vondracek <tomas.vondracek@gmail.com>
2010-11-02 23:52:02 +0100

    menu in news activity

--- StockAnalyze/src/cz/tomas/StockAnalyze/NewsActivity.java
+++ StockAnalyze/src/cz/tomas/StockAnalyze/NewsActivity.java
@@ -16,6 +16,10 @@ import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
@@ -34,59 +38,37 @@ public class NewsActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 
 		//progressDialog = ProgressDialog.show(this, "Please wait...", "Retrieving data ...", true);
-		ArrayAdapter<Article> adapter = new NewsListAdapter(this, R.layout.news_layout);
-		this.setListAdapter(adapter);
+		fill();
 		this.getListView().setTextFilterEnabled(true);
 		this.setContentView(R.layout.news_layout);
 		//fill();
 	}
 
 	private void fill() {
-		final List<String> newsItems = new ArrayList<String>();
-		
-		Runnable updateNews = new Runnable() {			
-			@Override
-			public void run() {
-				try {
-					RSSHandler rss = new RSSHandler();
-					NewsActivity.this.news = new NewsSqlHelper(NewsActivity.this);
-					
-					List<Feed> feeds = NewsActivity.this.news.getFeeds();
-					for (Feed feed : feeds) {
-						NewsActivity.this.news.deleteAricles(feed.getFeedId());
-						rss.updateArticles(NewsActivity.this, feed);
-						List<Article> articles = NewsActivity.this.news.getArticles(feed.getFeedId());
-						
-						for (Article a : articles) {
-							newsItems.add(a.getTitle());
-						}
-					}
-					
-					Runnable updateUi = new Runnable() {
-						
-						@Override
-						public void run() {
-							if (newsItems.size() == 0)
-								Toast.makeText(NewsActivity.this, R.string.FailedGetNews, Toast.LENGTH_LONG);
-							else {
-								NewsActivity.this.setListAdapter(new ArrayAdapter<String>(NewsActivity.this, R.layout.news_layout, newsItems));
-								NewsActivity.this.getListView().setTextFilterEnabled(true);
-								NewsActivity.this.progressDialog.dismiss();
-							}
-						}
-					};
-
-					rss.done();
-					NewsActivity.this.news.close();
-					
-					runOnUiThread(updateUi);
-				} catch (Exception e) {
-					Log.d("cz.tomas.StockAnalyze.NewsActivity", e.getMessage());
-				}
-			}			
-		};	
-
-		Thread thread = new Thread(null, updateNews, "updateNewsThread");
-		thread.start();
+		ArrayAdapter<Article> adapter = new NewsListAdapter(this, R.layout.news_layout);
+		this.setListAdapter(adapter);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.stock_list_menu, menu);
+	    return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.menu_stock_list_refresh:
+	    	this.setListAdapter(null);
+	    	this.findViewById(R.id.progressNews).setVisibility(View.VISIBLE);
+	    	this.fill();
+	        return true;
+	    case R.id.menu_stock_list_settings:
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 }
