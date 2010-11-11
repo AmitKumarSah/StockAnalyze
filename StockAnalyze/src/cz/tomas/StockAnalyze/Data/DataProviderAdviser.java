93cbdf0211bcda98d946a13aa5356d689e2e0472
Tomas Vondracek <tomas.vondracek@gmail.com>
2010-11-12 00:44:20 +0100

    Market class to represent Market adnd DataProviderAdviser class to help determine which DataProvider use

---
+++ StockAnalyze/src/cz/tomas/StockAnalyze/Data/DataProviderAdviser.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package cz.tomas.StockAnalyze.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.tomas.StockAnalyze.Data.Model.Market;
+
+/**
+ * @author tomas
+ * class advising about data provider, it is used in DataProviderFactory 
+ * to determine which concrete provider to use
+ */
+public class DataProviderAdviser {
+
+	boolean isRealTime;
+	boolean supportHistorical;
+	boolean supportStatistics;
+	List<Market> markets;
+	
+	
+	public DataProviderAdviser(boolean isRealTime, boolean supportHistorical,
+			boolean supportStatistics, List<Market> markets) {
+		this.isRealTime = isRealTime;
+		this.supportHistorical = supportHistorical;
+		this.supportStatistics = supportStatistics;
+		this.markets = markets;
+	}
+	
+	public DataProviderAdviser(boolean isRealTime, boolean supportHistorical,
+			boolean supportStatistics, Market market) {
+		this.isRealTime = isRealTime;
+		this.supportHistorical = supportHistorical;
+		this.supportStatistics = supportStatistics;
+		this.markets = new ArrayList<Market>();
+		this.markets.add(market);
+	}
+
+	/**
+	 * true if provider supports real time data (including delayed)
+	 * @return the isRealTime
+	 */
+	public boolean isRealTime() {
+		return isRealTime;
+	}
+
+	/**
+	 * true if provider supports historical data
+	 * @return the supportHistorical
+	 */
+	public boolean supportHistorical() {
+		return supportHistorical;
+	}
+
+	/**
+	 * true if provider supports statistical information (year max/min, ...)
+	 * @return the supportStatistics
+	 */
+	public boolean supportStatistics() {
+		return supportStatistics;
+	}
+
+	/**
+	 * Markets the provider covers
+	 * @return the market
+	 */
+	public List<Market> getMarkets() {
+		return markets;
+	}
+
+	
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (isRealTime ? 1231 : 1237);
+		result = prime * result + ((markets == null) ? 0 : markets.hashCode());
+		result = prime * result + (supportHistorical ? 1231 : 1237);
+		result = prime * result + (supportStatistics ? 1231 : 1237);
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DataProviderAdviser other = (DataProviderAdviser) obj;
+		if (isRealTime != other.isRealTime)
+			return false;
+		if (markets == null) {
+			if (other.markets != null)
+				return false;
+		} else if (!markets.equals(other.markets))
+			return false;
+		if (supportHistorical != other.supportHistorical)
+			return false;
+		if (supportStatistics != other.supportStatistics)
+			return false;
+		return true;
+	}
+
+	/* 
+	 * string representation
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "DataProviderAdviser [isRealTime=" + isRealTime
+				+ ", supportHistorical=" + supportHistorical
+				+ ", supportStatistics=" + supportStatistics + ", market count="
+				+ markets.size() + "]";
+	}
+	
+	
+}
---
+++ StockAnalyze/src/cz/tomas/StockAnalyze/Data/Model/Market.java
@@ -0,0 +1,161 @@
+/**
+ * 
+ */
+package cz.tomas.StockAnalyze.Data.Model;
+
+import java.io.Serializable;
+import java.util.Currency;
+
+/**
+ * @author tomas
+ * 
+ * represents market (e.g. NYSE, PSE,...)
+ * is serializable so it can be pu in an Intent
+ * 
+ * StockItem has a reference to the Market
+ */
+public class Market implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	String name;
+	String id;
+	Currency currency;
+	String description;
+
+	public Market(String name, String id, String currencyCode, String description) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.currency = Currency.getInstance(currencyCode);
+		this.description = description;
+	}
+	
+	public Market(String name, String id, Currency currency, String description) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.currency = currency;
+		this.description = description;
+	}
+	/**
+	 * name of market (e.g. New York Stock Exchange)
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * set name of market
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * unique code of the market
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * set unique code of the market
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * Currency used for trading on the market
+	 * @return the currency
+	 */
+	public Currency getCurrency() {
+		return currency;
+	}
+	/**
+	 * set Currency used for trading on the market
+	 * @param currency the currency to set
+	 */
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+	/**
+	 * market description, it's optional
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+
+	
+	/* 
+	 * hash code of this market object
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((currency == null) ? 0 : currency.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((currency == null) ? 0 : currency.getCurrencyCode().hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Market other = (Market) obj;
+		if (currency == null) {
+			if (other.currency != null)
+				return false;
+		} else if (!currency.equals(other.currency))
+			return false;
+//		if (description == null) {
+//			if (other.description != null)
+//				return false;
+//		} else if (!description.equals(other.description))
+//			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	/* string representation
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Market [name=" + name + ", id=" + id + ", currency=" + currency
+				+ ", description=" + description + "]";
+	}
+}
