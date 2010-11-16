ba750024a37915fe97aae463ca24b7912f1c09ab
Tomas Vondracek <tomas.vondracek@gmail.com>
2010-11-16 21:18:21 +0100

    application ico

--- StockAnalyze/gen/cz/tomas/StockAnalyze/R.java
+++ StockAnalyze/gen/cz/tomas/StockAnalyze/R.java
@@ -15,8 +15,9 @@ public final class R {
         public static final int list_item_background=0x7f040000;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
-        public static final int px_logo=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int px_logo=0x7f020002;
     }
     public static final class id {
         public static final int DetailLinear=0x7f070004;
