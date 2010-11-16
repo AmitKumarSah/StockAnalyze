b1281f06796f39f9b19db81cb3914fd72f2b295c
Tomas Vondracek <tomas.vondracek@gmail.com>
2010-11-16 21:46:32 +0100

    menu icons

--- StockAnalyze/gen/cz/tomas/StockAnalyze/R.java
+++ StockAnalyze/gen/cz/tomas/StockAnalyze/R.java
@@ -16,8 +16,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int px_logo=0x7f020002;
+        public static final int ic_menu_home=0x7f020001;
+        public static final int ic_menu_refresh=0x7f020002;
+        public static final int ic_menu_settings=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int px_logo=0x7f020005;
     }
     public static final class id {
         public static final int DetailLinear=0x7f070004;
