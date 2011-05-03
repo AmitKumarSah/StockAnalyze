8128442b0fb53910115c6b76482fdc26aa74f274
Tomas Vondracek <tomas.vondracek@gmail.com>
2011-05-04 00:07:16 +0200

    update scheduler uses the same intents for all the time

--- StockAnalyze/src/cz/tomas/StockAnalyze/UpdateScheduler.java
+++ StockAnalyze/src/cz/tomas/StockAnalyze/UpdateScheduler.java
@@ -14,7 +14,6 @@ import cz.tomas.StockAnalyze.receivers.AlarmReceiver;
 import cz.tomas.StockAnalyze.utils.FormattingUtils;
 import cz.tomas.StockAnalyze.utils.Utils;
 import android.app.AlarmManager;
-import android.app.Application;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -24,6 +23,18 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 /**
+ * UpdateScheduler service. allows to schedule different updates,
+ * according to application preferences and also invoke data update.
+ * This class is instantiated in Application class
+ * and is accessible as system service: 
+ * <p><code>
+ * UpdateScheduler scheduler = (UpdateScheduler) context.getSystemService(Application.UPDATE_SCHEDULER_SERVICE);
+ * </code>
+ * </p>
+ * 
+ * This class uses the same PendingIntent for sheduling to ensure,
+ * there aren't more scheduled alarms of the same type
+ * 
  * @author tomas
  *
  */
@@ -32,26 +43,35 @@ public class UpdateScheduler {
 	private Context context;
 	private SharedPreferences preferences;
 	
-	//private static UpdateScheduler instance;
-	
 	private final int DEFAULT_REFRESH_INTERVAL = 10;		//minutes
 	private final int REQUEST_CODE = 13215564;
 	
+	public static final String INTRA_UPDATE_ACTION = "cz.tomas.StockAnalyze.INTRADAY_DATA_UPDATE";
+	public static final String DAY_UPDATE_ACTION = "cz.tomas.StockAnalyze.DAY_DATA_UPDATE";
+	
 	private final int DAY_UPDATE_HOUR = 18;
 	
-	private boolean isSchedulerRunnig = false;
+	private boolean isSchedulerRunning = false;
 	
-//	/*
-//	 * singleton
-//	 */
-//	public static UpdateScheduler getInstance(Context context) {
-//		if (instance == null)
-//			instance = new UpdateScheduler(context);
-//		return instance;
-//	}
+	private static PendingIntent intraUpdateIntent;
+	private static PendingIntent dayUpdateIntent;
 	
 	UpdateScheduler(Context context) {
 		this.context = context;
+		
+		if (intraUpdateIntent == null) {
+			Intent intent = new Intent(INTRA_UPDATE_ACTION, null, this.context, AlarmReceiver.class);
+			intent.putExtra("intraday", true);
+			intraUpdateIntent = PendingIntent.getBroadcast(this.context,
+					REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		}
+		if (dayUpdateIntent == null) {
+			Intent intent = new Intent(DAY_UPDATE_ACTION, null, this.context, AlarmReceiver.class);
+			intent.putExtra("intraday", false);
+			dayUpdateIntent = PendingIntent.getBroadcast(this.context,
+					REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		}
+		Log.i(Utils.LOG_TAG, "update intents equality: " + dayUpdateIntent.equals(intraUpdateIntent));
 		this.preferences = context.getSharedPreferences(Utils.PREF_NAME, 0);
 		
 		this.preferences.registerOnSharedPreferenceChangeListener(new OnSharedPreferenceChangeListener() {
@@ -72,11 +92,11 @@ public class UpdateScheduler {
 		});
 	}
 	
-	public boolean isSchedulerRunnig() {
-		return isSchedulerRunnig;
+	boolean isSchedulerRunnig() {
+		return isSchedulerRunning;
 	}
 
-	/*
+	/**
 	 * Schedule next update with real time data provider,
 	 * if it is enabled in preferences
 	 */
@@ -84,7 +104,7 @@ public class UpdateScheduler {
 		this.scheduleNextIntraDayUpdate(MarketFactory.getCzechMarket());
 	}
 	
-	/*
+	/**
 	 * Schedule next update with real time data provider for given market,
 	 * if it is enabled in preferences
 	 */
@@ -94,7 +114,7 @@ public class UpdateScheduler {
 			this.scheduleAlarm(true);
 	}
 	
-	/*
+	/**
 	 * schedule next update with historical data provider
 	 */
 	public void scheduleNextDayUpdate() {
@@ -102,7 +122,7 @@ public class UpdateScheduler {
 			this.scheduleAlarm(false);
 	}
 	
-	/*
+	/**
 	 * update real time data immediately
 	 */
 	public void updateImmediatly() {
@@ -110,7 +130,7 @@ public class UpdateScheduler {
 			Log.i(Utils.LOG_TAG, "Device is offline, canceling data update");
 			return;
 		}
-		this.isSchedulerRunnig = true;
+		this.isSchedulerRunning = true;
 		if (!DataManager.isInitialized()) {
 			DataManager.getInstance(this.context);
 		}
@@ -120,36 +140,36 @@ public class UpdateScheduler {
 		task.execute(provider);
 	}
 	
-	/*
+	/**
 	 * schedule next update via Alarm and according to preferences
 	 */
 	private void scheduleAlarm(boolean intraDay) {
 		// get a Calendar object with current time
 		Calendar cal = Calendar.getInstance(Utils.PRAGUE_TIME_ZONE);
+		PendingIntent pendingIntent = null;
 		if (intraDay) {
 			int seconds = this.preferences.getInt(
 					Utils.PREF_INTERVAL_BACKGROUND_UPDATE,
 					DEFAULT_REFRESH_INTERVAL) * 60;
 			cal.add(Calendar.SECOND, seconds);
+			pendingIntent = intraUpdateIntent;
 		} else {
 			// set it for tomorrow evening
 			if (cal.get(Calendar.HOUR_OF_DAY) >= DAY_UPDATE_HOUR)
 				cal.add(Calendar.HOUR_OF_DAY, 24);
 			cal.set(Calendar.HOUR_OF_DAY, DAY_UPDATE_HOUR);
 			cal.set(Calendar.MINUTE,1);
+			pendingIntent = dayUpdateIntent;
 		}
 		Log.d(Utils.LOG_TAG, "SHEDULING " + (intraDay? "intraday" : "day") + " ALARM TO " + FormattingUtils.formatStockDate(cal));
-		
-		Intent intent = new Intent(this.context, AlarmReceiver.class);
-		intent.putExtra("intraday", intraDay);
-		PendingIntent sender = PendingIntent.getBroadcast(this.context, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			
 
 		// Get the AlarmManager service
 		AlarmManager am = (AlarmManager) this.context.getSystemService(Context.ALARM_SERVICE);
-		am.set(AlarmManager.RTC, cal.getTimeInMillis(), sender);
+		am.set(AlarmManager.RTC, cal.getTimeInMillis(), pendingIntent);
 	}
 	
-	/*
+	/**
 	 * do the refresh on given StockDataProvider
 	 */
 	class RefreshTask extends AsyncTask<IStockDataProvider, Integer, Boolean> {
@@ -168,7 +188,7 @@ public class UpdateScheduler {
 				} catch (Exception e) {
 					Log.e(Utils.LOG_TAG, "failed to refresh for provider", e);
 				}
-			isSchedulerRunnig = false;
+			isSchedulerRunning = false;
 			return null;
 		}
 		
