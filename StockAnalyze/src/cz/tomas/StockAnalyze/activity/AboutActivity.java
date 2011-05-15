5cf2d55b8f11a3a1c27226b745ccc003a0ffc13f
Tomas Vondracek <tomas.vondracek@gmail.com>
2011-05-15 20:34:52 +0200

    version code, showing version string in about screem

--- StockAnalyze/src/cz/tomas/StockAnalyze/activity/AboutActivity.java
+++ StockAnalyze/src/cz/tomas/StockAnalyze/activity/AboutActivity.java
@@ -5,8 +5,15 @@ package cz.tomas.StockAnalyze.activity;
 
 import cz.tomas.StockAnalyze.R;
 import cz.tomas.StockAnalyze.R.layout;
+import cz.tomas.StockAnalyze.utils.Utils;
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 /**
  * @author tomas
@@ -22,6 +29,17 @@ public class AboutActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		this.setContentView(R.layout.about_layout);
+		
+		PackageManager manager = this.getPackageManager();
+		try {
+			PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+
+			TextView versionText = (TextView) this.findViewById(R.id.aboutVersion);
+			String version = info.versionName;
+			versionText.setText(version);
+		} catch (Exception e) {
+			Log.e(Utils.LOG_TAG, "failed to retrieve appliation version", e);
+		}
 	}
 	
 	
