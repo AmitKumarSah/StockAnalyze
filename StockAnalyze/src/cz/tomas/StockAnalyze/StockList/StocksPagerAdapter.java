6aac03140781b3abe67e052a47e01b5e0b0c05ba
Tomas Vondracek <tomas.vondracek@gmail.com>
2012-06-25 23:42:04 +0200

    fixed issues with fragments in viewpager after markets settings change

--- StockAnalyze/src/cz/tomas/StockAnalyze/StockList/StocksPagerAdapter.java
+++ StockAnalyze/src/cz/tomas/StockAnalyze/StockList/StocksPagerAdapter.java
@@ -1,24 +1,40 @@
 package cz.tomas.StockAnalyze.StockList;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import cz.tomas.StockAnalyze.Data.Model.Market;
 import cz.tomas.StockAnalyze.fragments.StockGridFragment;
 import cz.tomas.StockAnalyze.fragments.StockListFragment;
+import cz.tomas.StockAnalyze.utils.Utils;
 
 /**
- * adapter creating {@link StockListFragment} for selected Markets
+ * Adapter creating {@link StockListFragment} for selected Markets.
+ * <br/>
+ * Most of the code is taken from {@link android.support.v4.app.FragmentPagerAdapter},
+ * but we changed the construction of fragment tag so we can add and remove fragments when user changes
+ * market settings.
  * @author tomas
  *
  */
-public final class StocksPagerAdapter extends FragmentPagerAdapter {
+public final class StocksPagerAdapter extends PagerAdapter {
+
+	private static final boolean DEBUG = Utils.DEBUG;
+	private static final String TAG = Utils.LOG_TAG;
 
 	private Market[] markets;
+	private final FragmentManager mFragmentManager;
+	private FragmentTransaction mCurTransaction = null;
+	private Fragment mCurrentPrimaryItem = null;
 	
 	public StocksPagerAdapter(FragmentManager fm) {
-		super(fm);
+		mFragmentManager = fm;
 	}
 	
 	public void setMarkets(Market[] markets) {
@@ -45,8 +61,7 @@ public final class StocksPagerAdapter extends FragmentPagerAdapter {
 		}
 		return this.markets[position];
 	}
-	
-	@Override
+
 	public Fragment getItem(int position) {
 		//StockListFragment fragment = new StockListFragment();
 		Market market = this.getMarketByPosition(position);
@@ -55,6 +70,68 @@ public final class StocksPagerAdapter extends FragmentPagerAdapter {
 		bundle.putSerializable(StockListFragment.ARG_MARKET, market);
 		fragment.setArguments(bundle);
 		return fragment;
+	}
+
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		if (mCurTransaction == null) {
+			mCurTransaction = mFragmentManager.beginTransaction();
+		}
+
+		// Do we already have this fragment?
+		Market market = this.getMarketByPosition(position);
+		String name = makeFragmentName(container.getId(), position, market);
+		Fragment fragment = mFragmentManager.findFragmentByTag(name);
+		if (fragment != null) {
+			if (DEBUG) Log.v(TAG, "Attaching item #" + position + ": f=" + fragment);
+			mCurTransaction.attach(fragment);
+		} else {
+			fragment = getItem(position);
+			if (DEBUG) Log.v(TAG, "Adding item #" + position + ": f=" + fragment);
+			mCurTransaction.add(container.getId(), fragment,
+					makeFragmentName(container.getId(), position, market));
+		}
+		if (fragment != mCurrentPrimaryItem) {
+			fragment.setMenuVisibility(false);
+			fragment.setUserVisibleHint(false);
+		}
+
+		return fragment;
+	}
+
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		if (mCurTransaction == null) {
+			mCurTransaction = mFragmentManager.beginTransaction();
+		}
+		if (Utils.DEBUG) Log.v(TAG, "Detaching item #" + position + ": f=" + object
+				+ " v=" + ((Fragment)object).getView());
+		mCurTransaction.detach((Fragment)object);
+	}
+
+	@Override
+	public void setPrimaryItem(ViewGroup container, int position, Object object) {
+		Fragment fragment = (Fragment)object;
+		if (fragment != mCurrentPrimaryItem) {
+			if (mCurrentPrimaryItem != null) {
+				mCurrentPrimaryItem.setMenuVisibility(false);
+				mCurrentPrimaryItem.setUserVisibleHint(false);
+			}
+			if (fragment != null) {
+				fragment.setMenuVisibility(true);
+				fragment.setUserVisibleHint(true);
+			}
+			mCurrentPrimaryItem = fragment;
+		}
+	}
+
+	@Override
+	public void finishUpdate(ViewGroup container) {
+		if (mCurTransaction != null) {
+			mCurTransaction.commitAllowingStateLoss();
+			mCurTransaction = null;
+			mFragmentManager.executePendingTransactions();
+		}
 	}
 
 	@Override
@@ -71,4 +148,22 @@ public final class StocksPagerAdapter extends FragmentPagerAdapter {
 		return market.getName().toUpperCase();
 	}
 
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return ((Fragment)object).getView() == view;
+	}
+
+	@Override
+	public Parcelable saveState() {
+		return null;
+	}
+
+	@Override
+	public void restoreState(Parcelable state, ClassLoader loader) {
+	}
+
+
+	private static String makeFragmentName(int viewId, int position, Market market) {
+		return String.format("android:switcher:%d index %d:%s", viewId, position, market.toString());
+	}
 }
